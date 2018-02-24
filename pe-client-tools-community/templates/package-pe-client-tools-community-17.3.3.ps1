be2080865e571a734949d0f2c0f948b6c781ffb8
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-02-24 03:02:59 +0000

    New Packages added by Appveyor 2018-02-24-03:02:59+00:00

---
+++ pe-client-tools-community/templates/package-pe-client-tools-community-17.3.3.ps1
@@ -0,0 +1,8 @@
+$PackageDefinition = @{
+  "TemplateName" = "pe-client-tools-community-v0.0.2";
+  "PackageName" = "pe-client-tools-community";
+  "PackageVersion" = "17.3.3";
+  "DownloadURL" = "https://pm.puppetlabs.com/pe-client-tools/2017.3.4/17.3.3/repos/windows/pe-client-tools-17.3.3-x64.msi";
+  "MD5Checksum" = "a3305a1cf5e805c5fc1a7a93e2df70ae";
+  "SHA256Checksum" = "433918a21425b71a5208f90a797074e3d22e95a451dfc326b70d48906e48f319";
+}
